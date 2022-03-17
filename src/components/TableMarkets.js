3c14834543a6a1e82a73b0d8c454a8a263c934a2
FournyP <pierre.fourny76@gmail.com>
2022-03-17 10:23:50 +0100

    Move TableMarket into a hooks

--- src/components/TableMarkets.js
+++ src/components/TableMarkets.js
@@ -1,64 +1,80 @@
-import React from 'react';
-import { Component } from "react";
+import React from "react";
 
-import Table from 'react-bootstrap/Table';
-import Badge from 'react-bootstrap/Badge';
+import Table from "react-bootstrap/Table";
+import Badge from "react-bootstrap/Badge";
 
-class TableMarkets extends Component {
+function TableMarkets(props) {
+  const [tokens, setTokens] = React.useState([]);
+  const [rates, setRates] = React.useState([]);
 
-    constructor(props) {
-        super(props)
-          this.state = {
-            tokens: []
-        }
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    if (isLoading) {
+      fetch("https://babyloans-api.herokuapp.com/api/tokens", {
+        mode: "cors",
+      }).then((tokenResponse) => {
+        setTokens(tokenResponse.json());
+
+        fetch("https://babyloans-api.herokuapp.com/api/rates", {
+          mode: "cors",
+        }).then((rateResponse) => {
+          setRates(rateResponse.json());
+        });
+      });
     }
+  }, [isLoading]);
 
-    componentDidMount() {
-        fetch('https://babyloans-api.herokuapp.com/api/tokens', {mode:'cors'})
-            .then((response) => {
-                return response.json()
-            })
-            .then((result) => {
-                this.setState({tokens: result['hydra:member']})
-        })
-
-    }
-
-    render() {
-        return (
-            <Table id="markets-table-rate-tokens" striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                    <th></th>
-                    <th>ASSETS</th>
-                    <th>TOTAL SUPPLY</th>
-                    <th>TOTAL BORROW</th>
-                    <th>STATUS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.tokens.map((result) => {
-                        return (
-                            <tr>
-                                <td className="markets-table-td-1"><img src={ process.env.PUBLIC_URL + "/tokens/" + result.logoUrl } className="markets-logo-assets"/></td>
-                                <td className="markets-table-td-1"><b>{result.symbol}</b></td>
-                                <td className="markets-table-td-1"></td>
-                                <td className="markets-table-td-1"></td>
-                                <td> 
-                                    { result.isActive 
-                                        ? <Badge pill bg="success">Available</Badge>
-                                        : <Badge pill bg="danger">Unavailable</Badge>
-                                    }
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </Table>     
-        );
-    }
+  return (
+    <Table id="markets-table-rate-tokens" striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th></th>
+          <th>ASSETS</th>
+          <th>TOTAL SUPPLY</th>
+          <th>TOTAL BORROW</th>
+          <th>STATUS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokens.map((result) => {
+          return (
+            <tr>
+              <td className="markets-table-td-1">
+                <img
+                  src={process.env.PUBLIC_URL + "/tokens/" + result.logoUrl}
+                  className="markets-logo-assets"
+                />
+              </td>
+              <td className="markets-table-td-1">
+                <b>{result.symbol}</b>
+              </td>
+              <td className="markets-table-td-1"></td>
+              <td className="markets-table-td-1"></td>
+              <td>
+                {rates.map((rate) => {
+                  if (rate.tokenId === result.id) {
+                    return <Badge variant="success">{rate.rate}</Badge>;
+                  }
+                })}
+              </td>
+              <td>
+                {result.isActive ? (
+                  <Badge pill bg="success">
+                    Available
+                  </Badge>
+                ) : (
+                  <Badge pill bg="danger">
+                    Unavailable
+                  </Badge>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
 
 export default TableMarkets;
